b1033e79d854210fdb96071aa027533f66a08bef
Yassine Zouin <opmail999@gmail.com>
2024-05-30 18:41:34 +0100

    added my mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("n", "$$", ":cd %:h<cr>")
+map("n", "$d", ":Dashboard<cr>")
+map("n", "$f", ":Telescope live_grep<cr>")
+map("n", "$s", ":DBUI<cr>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
